bc017e07d4a4e64e9c0233305a071f812839898a
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-04-03 20:21:44 +0900

    practice3

---
+++ Data_Science(2-1)/Week6/1020.py
@@ -0,0 +1,18 @@
+import sys
+import pandas as pd
+
+lst = []
+while True:
+    ss = sys.stdin.readline().rstrip()
+    lst.append(ss)
+    if ss == '':
+        break
+for i, v in enumerate(lst):
+    lst[i] = v.split()
+del lst[-1]
+df = pd.DataFrame(lst).T
+df.columns = ['Name','Age', 'Gender']
+df['Age'] = df['Age'].apply(int)
+df.index = df.pop('Name')
+df.index.name = 'Name'
+print(df.sort_values('Age', ascending=False))
