0b6b10463d8c137450b22eefa8a631fb1c2096d2
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-05-16 17:20:56 +0900

    update recent version

--- Data_Structure(2-1)/Week10/trees_codes/trees_codes/map_base.py
+++ Data_Structure(2-1)/Week10/trees_codes/trees_codes/map_base.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from collections import MutableMapping
+from collections.abc import MutableMapping
 
 class MapBase(MutableMapping):
   """Our own abstract base class that includes a nonpublic _Item class."""
