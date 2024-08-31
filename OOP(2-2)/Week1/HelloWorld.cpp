ece0e77200dbdbb12c7d6385987fe33b8e730443
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-08-31 20:50:26 +0900

    C++ environment setting

---
+++ OOP(2-2)/Week1/HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // include the iostream library
+using namespace std; // use the standard namespace
+int main() {
+    int num1, num2;
+    num1 = 1; num2 = 2;
+    // print a string "Hello World!" to the console
+    std::cout << "Hello World!" << endl;
+    // print the sum of num1 and num2 to the console
+    cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+    return 0 ;
+}
