f1b86129c2e9fe92e13b2e5a174b120bcecfe9a3
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-09-18 14:38:39 +0900

    array vs string

---
+++ OOP(2-2)/Week3/char_arrvssting.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    char cArr[] = "Hello, World!";
+    string str = "Hello, World!";
+    cout << cArr << size(cArr) << endl;
+    cout << str << size(str) << endl;
+    return 0;
+}
