d2b499445ca2e44708d2443a2f602655b2bb5e06
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-09-21 10:11:45 +0900

    deallocate

---
+++ OOP(2-2)/Week3/deallocate.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int** p = new int*[3];
+    for (int i = 0; i < 3; i++) {
+        p[i] = new int[3];
+    }
+    for (int i = 0; i < 3; i++) {
+        delete[] p[i];
+    }
+    delete[] p;
+}
