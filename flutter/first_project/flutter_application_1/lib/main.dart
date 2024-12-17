be9f1a59cccc1b26fedf4282bc84eb380c8bc970
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-12-17 14:45:43 +0900

    header

--- flutter/first_project/flutter_application_1/lib/main.dart
+++ flutter/first_project/flutter_application_1/lib/main.dart
@@ -11,12 +11,37 @@ class App extends StatelessWidget{ // StatelessWidget : widget without state
   Widget build(BuildContext context) {
     return MaterialApp( // MaterialApp : google app style(recommended), CupertinoApp : apple app style
       home: Scaffold( // Scaffold : main container
-        appBar: AppBar( // AppBar : top bar
-          title: Text('Hello World'), // Text : text widget
+        backgroundColor: Color(0xFF181818), // Color : 0xFF + hex color code
+        body: Padding(padding: EdgeInsets.symmetric(horizontal: 40), // Padding : padding
+          child: Column( // Column : vertical layout
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end, // horizontal alignment
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end, // vertical alignment
+                  children: [
+                    Text('Hey, Tecketing',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w800,
+                      )),
+                    Text('Welcome back',
+                      style: TextStyle(
+                        color: Colors.white.withValues(alpha: 0.8), // Define opacity value
+                        fontSize: 16,
+                      )),
+                  ],
+                )
+              ],
+            )
+          ],
         ),
-        body: Center( // Center : center align
-          child: Text('Hello World'), // Text : text widget
-        ),
+      )
       ),
     );
   }
