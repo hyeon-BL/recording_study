c69ad563532cd0185424bcc20114b29c01d9e7d3
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-12-19 17:05:41 +0900

    BuildContext, init/build/dispose

--- flutter/first_project/flutter_application_2/lib/main.dart
+++ flutter/first_project/flutter_application_2/lib/main.dart
@@ -13,8 +13,15 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  bool showTitle = true;
   // store widget data and ui state
   List<int> numbers = [];
+
+  void toggletitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
 
   void OnClick() {
     // update the state of the app
@@ -27,29 +34,67 @@ class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          titleLarge: TextStyle(color: Colors.red),
+        ),
+      ),
       home: Scaffold(
         backgroundColor: Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                'Click Count',
-                style: TextStyle(fontSize: 30),
+              showTitle ? MyLargeTitle() : Text('No Title'),
+              ElevatedButton(
+                onPressed: toggletitle,
+                child: Text('Toggle Title'),
               ),
-              for (var number in numbers)
-                Text(
-                  number.toString(),
-                  style: TextStyle(fontSize: 30),
-                ),
-              IconButton(
-                  // onPressed is a function that will be called when the button is clicked
-                  onPressed: OnClick,
-                  icon: const Icon(Icons.add_box_rounded)),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class MyLargeTitle extends StatefulWidget {
+  // seqarate widget -> don't apply state
+  const MyLargeTitle({
+    super.key,
+  });
+
+  @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int count = 0;
+
+  // init state(initialize) -> build -> dispose(delete widget tree)
+  @override
+  void initState() {
+    // called only once
+    super.initState();
+    print('initState');
+    count = 0;
+  }
+
+  @override
+  void dispose() {
+    // dispose the resources
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    // context has the information about the parents widget
+    return Text(
+      'My Large Title',
+      style: TextStyle(
+          fontSize: 30, color: Theme.of(context).textTheme.titleLarge?.color),
     );
   }
 }
