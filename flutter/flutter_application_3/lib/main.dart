8c871ce80bf52f3722a679ceb98146cd8eddedc3
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-12-20 15:47:11 +0900

    home screen

--- flutter/flutter_application_3/lib/main.dart
+++ flutter/flutter_application_3/lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp(
+      {super.key}); // key is a property of the widget to uniquely identify it
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ flutter/flutter_application_3/lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 1,
+        shadowColor: Colors.grey[100],
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Center(
+          child: Text(
+            '오늘의 웹툰',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+          ),
+        ),
+      ),
+    );
+  }
+}
