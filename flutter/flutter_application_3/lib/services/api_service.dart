3e6b647764b1d8d91e3e72f993298cc2553ce99f
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-12-20 16:19:49 +0900

    from Json to appliable format

--- flutter/flutter_application_3/lib/services/api_service.dart
+++ flutter/flutter_application_3/lib/services/api_service.dart
@@ -1,15 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter_application_3/model_model/webtoon.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
   final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
   final String today = "today";
 
-  void getTodayWebtoons() async {
+  Future<List<WebtoonModel>> getTodayWebtoons() async {
     // async function -> await until the function is done
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url); // future에서 response를 받을 때까지 기다림
     if (response.statusCode == 200) {
-      print(response.body);
+      // 200: OK
+      final List<dynamic> webtoons =
+          jsonDecode(response.body); // response.body: json string
+      for (var webtoon in webtoons) {
+        final toon = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(toon);
+      }
+      return webtoonInstances;
     } else {
       throw Exception('Failed to load webtoons');
     }
