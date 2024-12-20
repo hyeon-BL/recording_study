46388f6920d6b13a9ed4b8a12b71b01c63c4e2e8
Lee Hyeon Jun <lhbj1115@gmail.com>
2024-12-20 16:28:28 +0900

    recap

--- flutter/flutter_application_3/lib/model_model/webtoon.dart
+++ flutter/flutter_application_3/lib/model_model/webtoon.dart
@@ -1,7 +1,7 @@
 class WebtoonModel {
-  final String title, thumb, id;
+  late final String title, thumb, id;
 
-  WebtoonModel.fromJson(Map<String, dynamic> json)
+  WebtoonModel.fromJson(Map<String, dynamic> json) // json: Map<String, dynamic>
       : title = json['title'],
         thumb = json['thumb'],
         id = json['id'];
