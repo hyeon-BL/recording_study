018ccaf418262c544ddeb769682fecbdf65ab34d
Lee Hyeon Jun <lhbj1115@gmail.com>
2025-04-16 17:00:16 +0900

    add function to count the number of zeros

--- system_programming(3-1)/midterm_example/bit_operation2.c
+++ system_programming(3-1)/midterm_example/bit_operation2.c
@@ -10,10 +10,24 @@ int count_ones(unsigned int n)
     }
     return count;
 }
+
+int count_zeros(unsigned int n)
+{
+    unsigned int count = 0;
+    while (n > 0)
+    {
+        count += !(n & 1); // Increment count if the last bit is 0
+        n >>= 1;           // Right shift n by 1 to check the next bit
+    }
+    return count;
+}
+
 int main()
 {
     unsigned int n = 0xF0BD; // Example number (1111000010111101 in binary)
     unsigned int result = count_ones(n);
+    unsigned int result_zeros = count_zeros(n);
     printf("Number of 1's in %X: %d\n", n, result);
+    printf("Number of 0's in %X: %d\n", n, result_zeros);
     return 0;
 }
