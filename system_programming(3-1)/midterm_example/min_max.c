ac609f5e229309cd67bf240e8ef8b8a86d2b847a
Lee Hyeon Jun <lhbj1115@gmail.com>
2025-04-17 10:37:59 +0900

    understand how to calculate min & max value

---
+++ system_programming(3-1)/midterm_example/min_max.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i_max = 0x7FFFFFFF; // 2147483647 in decimal
+    int i_min = 0x80000000; // -2147483648 in decimal
+    printf("i_max: %d\n", i_max);
+    printf("i_min: %d\n", i_min);
+
+    int i_max2 = (1 << 31) - 1; // 2147483647 in decimal
+    printf("i_max2: %d\n", i_max2);
+
+    int i_max3 = 0b01111111111111111111111111111111; // 2147483647 in decimal
+    printf("i_max3: %d\n", i_max3);
+
+    unsigned int u_max = 0xFFFFFFFF; // 4294967295 in decimal
+    printf("u_max: %u\n", u_max);
+}
